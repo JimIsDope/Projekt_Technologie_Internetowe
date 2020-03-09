df2ec18ac9d7f6c493cb29ece75cabe0197a71a7
JimIsDope <59023370+JimIsDope@users.noreply.github.com>
2020-03-09 09:45:02 +0100

    Add files via upload
    Dodanie sekcji stopki bez zawartości(szablon), dodanie funkcji podmiany tekstu na stronie na angielski(nawigacja)

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+//Inserting hero image
 const hero = document.querySelector(".hero");
 const heroImage = document.createElement("img");
 heroImage.src = "images/hero-image.svg";
 hero.appendChild(heroImage);
+
+
+
+//Changing site's language
+const btnLang = document.querySelector('.language');
+const navList = document.querySelectorAll('.nav__li a');
+const changeLanguage = () =>
+{
+    navList[0].textContent = "Home";
+    navList[1].textContent = "About";
+    navList[2].textContent = "Skills";
+    navList[3].textContent = "Contact";
+    btnLang.textContent = "Polski";
+}
+btnLang.addEventListener('click', changeLanguage);
