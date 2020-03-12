9fb5e0dd71a99a242cfdbe65b34d0d11c24ee634
sebastiankulczycki <sebastian.kulczycki98@gmail.com>
2020-03-12 22:06:55 +0100

    Responsive navigation, added hamburger

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-//Changing site's language
-const btnLang = document.querySelector(".language");
-const navList = document.querySelectorAll(".nav__li a");
-const changeLanguage = () => {
-  navList[0].textContent = "Home";
-  navList[1].textContent = "About";
-  navList[2].textContent = "Skills";
-  navList[3].textContent = "Contact";
-  btnLang.textContent = "Polski";
+//Hamburger slider
+const navItemsSlide = () => {
+  const burger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav__items");
+  const navItems = document.querySelectorAll(".nav__items li");
+  //Toggle class active
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav__items-active");
+    burger.classList.toggle("hamburger-active");
+
+    //Animation
+    navItems.forEach((item, index) => {
+      item.style.animation = `navItemsFade 0.5s ease forwards ${index / 7 +
+        2}s`;
+      item.style.margin = 0;
+    });
+  });
 };
-btnLang.addEventListener("click", changeLanguage);
+
+//Changing site's language
+const changeLanguage = () => {
+  const btnLang = document.querySelector(".language");
+  const navList = document.querySelectorAll(".nav__li a");
+  btnLang.addEventListener("click", () => {
+    navList[0].textContent = "Home";
+    navList[1].textContent = "About";
+    navList[2].textContent = "Skills";
+    navList[3].textContent = "Contact";
+    btnLang.textContent = "Polski";
+  });
+};
+
+const App = () => {
+  navItemsSlide();
+  changeLanguage();
+};
+
+App();
