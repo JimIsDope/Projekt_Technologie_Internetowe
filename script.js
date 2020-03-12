e77113964774989742a9fb15280a6b2993c15bf0
sebastiankulczycki <sebastian.kulczycki98@gmail.com>
2020-03-13 00:17:20 +0100

    Added some fixes

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 //Hamburger slider
 const navItemsSlide = () => {
+  const burgerLines = document.querySelectorAll(".hamburger__line");
   const burger = document.querySelector(".hamburger");
   const nav = document.querySelector(".nav__items");
   const navItems = document.querySelectorAll(".nav__items li");
+  const btnLang = document.querySelector(".language");
   //Toggle class active
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav__items-active");
     burger.classList.toggle("hamburger-active");
-
+    burgerLines.forEach((item) => {
+      item.classList.toggle("hamburger__line-active");
+    });
+    btnLang.classList.toggle("language-mobile");
     //Animation
     navItems.forEach((item, index) => {
-      item.style.animation = `navItemsFade 0.5s ease forwards ${index / 7 +
-        0.7}s`;
-      item.style.margin = 0;
+      if (item.style.animation) {
+        item.style.animation = "";
+      } else {
+        item.style.animation = `navItemsFade 0.5s ease forwards ${index / 7 +
+          0.5}s`;
+        item.style.margin = 0;
+      }
     });
   });
 };
